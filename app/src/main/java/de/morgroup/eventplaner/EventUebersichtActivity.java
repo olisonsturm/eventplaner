cb5767c62fbb70e3919fee5ce7cf829631c649c9
Olison Sturm <olisonsturm@gmx.de>
2021-03-28 21:10:26 +0200

    Update EventUebersichtActivity

--- app/src/main/java/de/morgroup/eventplaner/EventUebersichtActivity.java
+++ app/src/main/java/de/morgroup/eventplaner/EventUebersichtActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class EventÜbersicht extends AppCompatActivity {
+public class EventUebersichtActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
