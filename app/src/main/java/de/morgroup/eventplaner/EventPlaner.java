7dc2eacd2579d1f729f006e2d605b534e3ee95fd
Olison Sturm <olisonsturm@gmx.de>
2021-11-11 20:22:05 +0100

    updated

--- app/src/main/java/de/morgroup/eventplaner/EventPlaner.java
+++ app/src/main/java/de/morgroup/eventplaner/EventPlaner.java
@@ -4,14 +4,28 @@ import android.app.Application;
 import android.view.textclassifier.TextClassifierEvent;
 
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.text.DateFormat;
+
 public class EventPlaner extends Application {
+
     @Override
     public void onCreate() {
         super.onCreate();
         // app initialization logic
         FirebaseAuth.getInstance().useAppLanguage();
         FirebaseApp.initializeApp(this);
+
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+
+        firebaseAppCheck.installAppCheckProviderFactory(
+                SafetyNetAppCheckProviderFactory.getInstance());
+
+        firebaseAppCheck.installAppCheckProviderFactory(
+                DebugAppCheckProviderFactory.getInstance());
     }
 }
